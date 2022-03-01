6805d4c7086e1c553449c954dc6c3cfead30fd0d
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-01 19:35:44 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,14 @@
+// Sum of elements of array
+
+#include<stdio.h>
+
+int main()
+{
+  int a[]= {11,22,36,5,2};
+  int sum =0,*p;
+  for(p=&a[0];p<= &a[4];p++)
+    sum += *p;
+
+  printf("sum is %d",sum);
+  return 0;
+}
